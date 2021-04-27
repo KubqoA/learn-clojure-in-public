8b5128f825b8532869f0430d34ed62d22ece1eca
Jakub Arbet <hi@jakubarbet.me>
2021-04-27 12:02:22 +0200

    exercism: allergies: solution

--- code/exercism/clojure/allergies/src/allergies.clj
+++ code/exercism/clojure/allergies/src/allergies.clj
@@ -1,9 +1,19 @@
 (ns allergies)
 
-(defn allergies [] ;; <- arglist goes here
-  ;; your code goes here
+(def allergens
+  {:eggs 1
+   :peanuts 2
+   :shellfish 4
+   :strawberries 8
+   :tomatoes 16
+   :chocolate 32
+   :pollen 64
+   :cats 128})
+
+(defn allergies [score]
+  (map first (filter #(> (bit-and score (second %)) 0) allergens))
 )
 
-(defn allergic-to? [] ;; <- arglist goes here
-  ;; your code goes here
+(defn allergic-to? [score allergy]
+  (> (bit-and score (allergens allergy)) 0)
 )
