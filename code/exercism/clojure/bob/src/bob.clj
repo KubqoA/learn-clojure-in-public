bbab9d39ba84182bd09e280bd9c0235b68f905d0
Jakub Arbet <hi@jakubarbet.me>
2021-04-27 11:41:16 +0200

    exercism: bob: solution

--- code/exercism/clojure/bob/src/bob.clj
+++ code/exercism/clojure/bob/src/bob.clj
@@ -1,5 +1,21 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
+(declare silence? yell? question? question-yell?)
+
+(defn response-for [s]
+  (let [s (str/trim s)]
+    (cond
+      (silence? s) "Fine. Be that way!"
+      (question-yell? s) "Calm down, I know what I'm doing!"
+      (question? s) "Sure."
+      (yell? s) "Whoa, chill out!"
+      :else "Whatever."
+    )
+  )
 )
+
+(defn- silence? [s] (str/blank? s))
+(defn- yell? [s] (and (re-find #"[A-Z]" s) (not (re-find #"[a-z]" s))))
+(defn- question? [s] (= \? (last s)))
+(defn- question-yell? [s] (or (str/ends-with? s "?!") (and (question? s) (yell? s))))
